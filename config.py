7e057866bde9eb64099312b5ef6d1dd422866576
flash-po <liqop960+1@gmail.com>
2024-08-30 02:23:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6587124267:AAHZHguNqkSttjTM0DprFmiXWdetAHF7pEo")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("ironmanarmy")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("819491004", "5898572118", "6052201320")
+
+
+
+
+
+
+
+  
